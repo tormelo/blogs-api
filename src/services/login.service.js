4a15dfb490e17e01d6741194390b160c01472b19
tormelo <23294699+tormelo@users.noreply.github.com>
2022-12-05 19:41:28 -0300

    add login service

---
+++ src/services/login.service.js
@@ -0,0 +1,20 @@
+const { createToken } = require('../auth');
+const { User } = require('../models');
+
+module.exports = async (loginInfo) => {
+  const { email, password } = loginInfo;
+
+  if (!email || !password) {
+    return ({ type: 'REQUIRED_FIELD', message: 'Some required fields are missing' });
+  }
+
+  const user = await User.findOne({ where: { email, password } });
+
+  if (!user) {
+    return ({ type: 'INVALID_FIELD', message: 'Invalid fields' });
+  }
+
+  const token = createToken(user.email);
+
+  return { type: '', message: token };
+};

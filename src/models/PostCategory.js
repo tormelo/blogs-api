c13cc0e17fd031a229700bfbe65ce4ace343f4c8
tormelo <23294699+tormelo@users.noreply.github.com>
2022-12-06 15:07:27 -0300

    add PostCategory model and associations

---
+++ src/models/PostCategory.js
@@ -0,0 +1,38 @@
+module.exports = (sequelize, DataTypes) => {
+  const PostCategory = sequelize.define(
+    'PostCategory',
+    {
+      postId: { 
+        type: DataTypes.INTEGER, 
+        primaryKey: true 
+      },
+      categoryId: { 
+        type: DataTypes.INTEGER, 
+        primaryKey: true 
+      },
+      
+    },
+    {
+      timestamps: false,
+      underscored: true,
+      tableName: 'posts_categories'
+    }
+  );
+
+  PostCategory.associate = (models) => {
+    models.BlogPost.belongsToMany(models.Category, {
+      as: 'categories',
+      through: PostCategory,
+      foreignKey: 'postId',
+      otherKey: 'categoryId',
+    });
+    models.Category.belongsToMany(models.BlogPost, {
+      as: 'posts',
+      through: PostCategory,
+      foreignKey: 'categoryId',
+      otherKey: 'postId',
+    });
+  };
+
+  return PostCategory;
+};

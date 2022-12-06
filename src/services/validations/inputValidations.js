dc162ff7da33b3782523bb405e0f606582faf0fe
tormelo <23294699+tormelo@users.noreply.github.com>
2022-12-06 10:17:38 -0300

    add user register validations

---
+++ src/services/validations/inputValidations.js
@@ -0,0 +1,31 @@
+const { userSchema } = require('./schemas');
+const { User } = require('../../models');
+
+const validateNewUserEmail = async (email) => {
+  const user = await User.findOne({ where: { email } });
+
+  if (user) {
+    return { type: 'ALREADY_IN_DB', message: 'User already registered' }; 
+  }
+};
+
+const validateNewUserInfo = (userInfo) => {
+  const { error } = userSchema.validate(userInfo);
+
+  if (error) { 
+    const { message } = error;
+    return { type: 'INVALID_FIELD', message }; 
+  }
+};
+
+const validateNewUser = async (userInfo) => {
+  const dataError = validateNewUserInfo(userInfo);
+  if (dataError) return dataError;
+
+  const emailError = await validateNewUserEmail(userInfo.email);
+  if (emailError) return emailError;
+
+  return { type: null, message: '' };
+};
+
+module.exports = { validateNewUser };
--- src/utils/errorMap.js
+++ src/utils/errorMap.js
@@ -1,6 +1,7 @@
 const errorMap = {
   INVALID_FIELD: 400,
   REQUIRED_FIELD: 400,
+  ALREADY_IN_DB: 409,
 };
 
 const mapError = (type) => errorMap[type] || 500;

d42512282e0fb8d0d5b5f4ee1b80be0fb67d8a20
tormelo <23294699+tormelo@users.noreply.github.com>
2022-12-06 10:23:04 -0300

    add user router

---
+++ src/routers/user.router.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const { userController } = require('../controllers');
+
+const router = express.Router();
+
+router.post(
+  '/',
+  userController.register,
+);
+
+module.exports = router;
